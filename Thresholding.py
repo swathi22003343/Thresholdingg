5e5140a465bafbeaf30ca074a2ceb5458c6e65c4
swathi22003343 <120440439+swathi22003343@users.noreply.github.com>
2024-10-02 14:31:06 +0530

    Update Thresholding.py

--- Thresholding.py
+++ Thresholding.py
@@ -12,7 +12,9 @@ import matplotlib.pyplot as plt
 
 #READ THE IMAGE USING IMREAD
 #CONVERT THE COLOR FROM BGR TO RGB
-image_gray=cv2.imread("DUCK.jpg",0)
+image = cv2.imread('8P.jpg',1)
+image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
+image_gray = cv2.imread('8P.jpg',0)
 
 # Use Global thresholding to segment the image
 ret,thresh_dipt1=cv2.threshold(image_gray,86,255,cv2.THRESH_BINARY)
@@ -34,7 +36,7 @@ thresh_dipt8=cv2.adaptiveThreshold(image_gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C
 
 titles=["Gray Image","Threshold Image (Binary)","Threshold Image (Binary Inverse)","Threshold Image (To Zero)"
        ,"Threshold Image (To Zero-Inverse)","Threshold Image (Truncate)","Otsu","Adaptive Threshold (Mean)","Adaptive Threshold (Gaussian)"]
-images=[image_gray,thresh_dipt1,...................,thresh_dipt8]#LIST OUT ALL TILL thresh_dipt8 i.e from thresh_dipt_1 till thresh_dipt_8a
+images=[image_gray,thresh_dipt1,thresh_dipt2,thresh_dipt3,thresh_dipt4,thresh_dipt5,thresh_dipt6,thresh_dipt7,thresh_dipt8]#LIST OUT ALL TILL thresh_dipt8 i.e from thresh_dipt_1 till thresh_dipt_8a
 for i in range(0,9):
     plt.figure(figsize=(10,10))
     plt.subplot(1,2,1)
